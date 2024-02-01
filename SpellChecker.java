ca2739508070e84a95305f39f6061568e8503d71
Raz Bouganim <raz.bouganim@post.runi.ac.il>
2024-02-01 21:46:14 +0200

    Raz's HomeWork 07

--- SpellChecker.java
+++ SpellChecker.java
@@ -12,10 +12,22 @@ public class SpellChecker {
 
 	public static String tail(String str) {
 		// Your code goes here
+		return str.substring(1 , str.length());
 	}
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		if (word1.length() == 0){
+			return word2.length();
+		} else if (word2.length() == 0) {
+			return word1.length();
+		} else if (word1.charAt(0) == word2.charAt(0)) {
+			return levenshtein((tail(word1)) , tail(word2));
+		} else {
+			return (1 + Math.min(levenshtein(tail(word1) , word2) , Math.min(levenshtein(word1 , tail(word2)) , levenshtein(tail(word1) , tail(word2)))));
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -24,12 +36,24 @@ public class SpellChecker {
 		In in = new In(fileName);
 
 		// Your code here
+		for (int i = 0; i < dictionary.length; i++){
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		String newWord = word;
+		int min = threshold + 1;
+		for (int i = 1; i < dictionary.length; i++){
+			if (levenshtein(word, dictionary[i]) < min){
+				min = levenshtein(word , dictionary[i]);
+				newWord = dictionary[i];
+			}
+		}
+		return newWord;
 	}
 
 }
